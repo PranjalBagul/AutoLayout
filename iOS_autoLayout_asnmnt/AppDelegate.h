f8c5dc815114084c85e02c33333044fa320f4989
Student P_08 <studentp_08@Student-P-08.local>
2017-09-14 13:21:10 +0530

    Initial Commit

---
+++ iOS_autoLayout_asnmnt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_autoLayout_asnmnt
+//
+//  Created by Student P_08 on 14/09/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
